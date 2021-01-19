d5ff94c810ecb072ca32058e41cd091104af3378
adacienciala <adacien@o2.pl>
2021-01-19 20:09:29 +0100

    added generating button

--- webpage/script.js
+++ webpage/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $('.tech-slideshow').hide();
+    $('.generate-button').hide();
     $('#logodiv').click(function(){
         $('#logodiv').fadeTo('slow', 0.3);
         $('.tech-slideshow').show('slow');
+        $('.generate-button').fadeIn('slow');
+    });
+
+    $('#button, #button-icon').click(function(){
+        console.log("click");
     });
 
     // TODO: slider dlugiej szerokosci i na koncu powtorzenie pierwszych
@@ -17,11 +23,6 @@ $(document).ready(function(){
         success: function (data) {
             //List all .png file names in the page
             var images = $(data).find("a:contains(" + fileextension + ")");
-            // document.querySelector('head').innerHTML += `<style>@keyframes moveSlideshowLeft {
-            //     100% { 
-            //         transform: translateX(calc(-128px * ${images.length/2}))
-            //     }
-            //   }</style>`;
             images.each(function (i) {
                 var filename = this.href.replace(window.location.host, "").replace("http://", "");
                 if (i < images.length/2) {
@@ -35,32 +36,14 @@ $(document).ready(function(){
         }
     });
 
-    // function wrap() {
-    //     setTimeout(function() {
-    //         var overflowedImg = $moverL.children(":first");
-    //         overflowedImg.appendTo($moverL);
-    //         setTimeout(function() {
-    //             wrap();
-    //         }, 200);
-    //     }, 800);
-    // }
-
-    // wrap();
-
-    $moverL.on('animationiteration', function (e) {
+    $moverL.on('webkitAnimationIteration oanimationiteration msAnimationIteration animationiteration', function (e) {
         var overflowedImg = $moverL.children(":first");
         overflowedImg.appendTo($moverL);
     })
 
-    $moverR.on('animationiteration', function (e) {
+    $moverR.on('webkitAnimationIteration oanimationiteration msAnimationIteration animationiteration', function (e) {
         var overflowedImg = $moverR.children(":last");
         overflowedImg.prependTo($moverR);
     })
 
-    // setInterval(function() {
-    //     var overflowedImg = $moverL.children(":first");
-    //     overflowedImg.remove();
-    //     overflowedImg.appendTo($moverL);
-    //     console.log("moved");
-    // }, 2000);
 });
